e5c785b5c7882080d74767a5e6f68770c7697c3d
KyuHyeon <160197431+stagechaplin@users.noreply.github.com>
2024-10-31 01:08:18 +0900

    Delete src directory

--- src/main/resources/static/js/delete.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('.delete-button').addEventListener('click', function() {
-  document.querySelectorAll('.delete-checkbox:checked').forEach(checkbox => {
-    checkbox.parentElement.remove();
-  })
-})
